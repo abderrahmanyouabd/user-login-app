bf9746c7eb7c4c7b331b672aafd7ac219687b759
Abderrahman Youabd <youabd50@gmail.com>
2024-07-12 07:03:01 +0200

    conf:  configuring jwt security

--- user-login/src/main/java/com/a1st/userlogin/service/JwtUtils.java
+++ user-login/src/main/java/com/a1st/userlogin/service/JwtUtils.java
@@ -56,6 +56,13 @@ public class JwtUtils {
         return claimsTFunction.apply(Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload());
     }
 
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+      final String username = extractUsername(token);
+      return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
 
+  private boolean isTokenExpired(String token) {
+    return extractClaims(token, Claims::getExpiration).before(new Date()); // todo I should prob do 'after'
+  }
 
 }
