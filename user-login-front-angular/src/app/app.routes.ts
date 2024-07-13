2e15c401e7662b9efb9c50de26b5e1933d8e7d08
Abderrahman Youabd <youabd50@gmail.com>
2024-07-13 19:58:36 +0200

    conf: configured the basic initial routing

--- user-login-front-angular/src/app/app.routes.ts
+++ user-login-front-angular/src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
+import {ProfileComponent} from "./profile/profile.component";
+import {UserListComponent} from "./user-list/user-list.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'users',
+    component: UserListComponent
+  },
+  {
+    path: '**',
+    component: LoginComponent
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+];
