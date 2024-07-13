ad6f3f9e65758c3995d4ed3da0a40c6fc7e7489f
Abderrahman Youabd <youabd50@gmail.com>
2024-07-13 19:57:07 +0200

    feat: designed basic page content for components

--- user-login-front-angular/src/app/nav/nav.component.ts
+++ user-login-front-angular/src/app/nav/nav.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {RouterLink, RouterOutlet} from "@angular/router";
+import {UsersService} from "../users.service";
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterOutlet
+  ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
-export class NavComponent {
+export class NavComponent implements OnInit{
+  isAuthenticated: boolean = false;
+  isAdmin: boolean = false;
+  isUser: boolean = false;
 
+  constructor(private readonly userService: UsersService) { }
+
+  ngOnInit(): void {
+        this.isAuthenticated = this.userService.isAuthenticated();
+        this.isAdmin = this.userService.isAdmin();
+        this.isUser = this.userService.isUser();
+    }
+
+  logout() {
+    this.userService.logOut();
+    this.isAuthenticated = false;
+    this.isAdmin = false;
+    this.isUser = false;
+
+  }
 }
